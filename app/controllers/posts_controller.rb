1acc2ec04c56de10c5ee0cd6f2abea1ca4240a3e
SFarida <faridagueleu@gmail.com>
2023-11-11 15:14:23 +0100

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params['title']
+    @post.text = params['text']
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
