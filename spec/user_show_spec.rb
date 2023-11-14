af096775246a819e23056e2853bb64462bed87d1
thasipablo <thasipablo@gmail.com>
2023-11-14 23:47:20 +0100

    add post show specs and fix user show spec code

--- spec/user_show_spec.rb
+++ spec/user_show_spec.rb
@@ -1,44 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe 'posts/show', type: :feature do
+RSpec.describe 'users/index', type: :feature do
   before(:each) do
-    @user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer',
-                        posts_counter: 0)
-    @first_post = Post.create(author: @user, title: 'Hello', text: 'This is my first post', comments_counter: 5,
-                              likes_counter: 0)
+    @user = User.create(name: 'John Doe', photo: 'https://i.pravatar.cc/300',
+                        bio: 'Alien biologist', posts_counter: 1)
     5.times do |i|
-      Comment.create(text: "This is comment ##{i}", user_id: @user.id, post_id: @first_post.id)
+      Post.create(title: "Post - #{i}", text: 'This is the first post',
+                  comments_counter: 5, likes_counter: 0, author_id: @user.id)
     end
-    visit user_post_path(@user.id, @first_post)
+    visit user_path(id: @user.id)
   end
 
-  it 'displays the post title' do
-    expect(page).to have_content('Hello')
+  it 'displays user profile photo' do
+    expect(page).to have_css("img[src*='https://i.pravatar.cc/300']")
   end
 
-  it 'displays the post body' do
-    expect(page).to have_content('This is my first post')
-  end
-
-  it 'displays the post author' do
-    expect(page).to have_content('by Lilly')
-  end
-
-  it 'displays the number of comments' do
-    expect(page).to have_content('Comments: 10')
-  end
-
-  it 'displays the number of likes' do
-    expect(page).to have_content('Likes: 0')
-  end
-
-  it 'displays the username of each comment' do
+  it 'displays username' do
     expect(page).to have_content(@user.name)
   end
 
-  it 'displays the body of each comment' do
-    expect(page).to have_content('This is comment #0')
-    expect(page).to have_content('This is comment #1')
-    expect(page).to have_content('This is comment #2')
+  it 'displays number of posts' do
+    expect(page).to have_content('Number of posts: 6')
+  end
+
+  it "displays the user's first 3 posts" do
+    expect(page).to have_content('Post - 4')
+    expect(page).to have_content('Post - 3')
+    expect(page).to have_content('Post - 2')
+  end
+
+  it 'displays the user bio' do
+    expect(page).to have_content('Alien biologist')
+  end
+
+  it 'displays a button to see all posts' do
+    expect(page).to have_content('See all posts')
+  end
+
+  it 'show all the user posts when the button is clicked' do
+    # click_on 'See all posts'
+    find(".all_posts > a[href='#{user_posts_path(@user)}']").click
+    expect(page).to have_content('Post - 0')
+    expect(page).to have_content('Post - 1')
+    expect(page).to have_content('Post - 2')
+    expect(page).to have_content('Post - 3')
+    expect(page).to have_content('Post - 4')
+  end
+
+  it 'Redirects to the user posts index page when clicking See all posts' do
+    # click_on 'See all posts'
+    visit user_posts_path(@user.id)
+    expect(page).to have_current_path(user_posts_path(@user.id))
   end
 end
